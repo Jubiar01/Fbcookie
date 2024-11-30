94ae026763fde35e36bf3e14fb164706dae788cf
Levitzy <levitzy123@gmail.com>
2024-11-30 14:56:13 +0800

    auto send bot result apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
 
 dependencies {
     implementation("com.google.firebase:firebase-analytics:22.1.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.code.gson:gson:2.11.0")
 }
